22c045ac199d9066fe9f74b38041081e0fa35aa2
Chris3918 <155273681+Chris3918@users.noreply.github.com>
2024-04-18 00:18:35 -0400

    fix 1

--- src/app/spotify/components/spotifyComponents.tsx
+++ src/app/spotify/components/spotifyComponents.tsx
@@ -1,8 +1,11 @@
 "use client";
-import React, { useState, useMemo, useCallback } from "react";
+import React, { useState, useMemo, useCallback, useEffect } from "react";
 import "./spotify.css";
 import { useGetTopArtistsFullList } from "@/app/spotify/hooks/spotifyHooks";
-
+import { TimeRangeRadioButtons } from "@/app/spotify/components/paginationList/TimeRangeRadioButtons";
+import { Pagination } from "@/app/spotify/components/paginationList/topArtistListPagination";
+import { TopArtistList } from "@/app/spotify/components/paginationList/topArtistList";
+import { TimeRangeSelector, TopArtists, ArtistPagination } from "@/app/spotify/components/topArtistList_functions";
 export function CardGrid() {
   return (
     <div className="force-tocenter">
@@ -20,19 +23,12 @@ export function CardGrid() {
   );
 }
 
-export function MyTopArtistCard() {
-  let time_range = "short_term";
+function MyTopArtistCard() {
+  const [time_range, setTimeRange] = useState("medium_term");
   const topArtistsFullList = useGetTopArtistsFullList(time_range);
   const pageSize = 10;
   const [currentPage, setCurrentPage] = useState(1);
   const totalPages = Math.ceil(topArtistsFullList.length / pageSize);
-  const startIndex = (currentPage - 1) * pageSize;
-
-  const currentArtists = useMemo(() => {
-    const startIndex = (currentPage - 1) * pageSize;
-    const endIndex = startIndex + pageSize;
-    return topArtistsFullList.slice(startIndex, endIndex);
-  }, [topArtistsFullList, currentPage, pageSize]);
   const startIndex = (currentPage - 1) * pageSize;
   const endIndex = startIndex + pageSize;
   const currentArtists = topArtistsFullList.slice(startIndex, endIndex);
@@ -49,131 +45,101 @@ export function MyTopArtistCard() {
     }
   }, [currentPage, totalPages]);
 
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [topArtistsFullList]);
+
   return (
     <div className="top-artist-container">
       <header className="top-artist-container-header">
         Top Artists
-        <div className="radio-button-container"></div>
+        <TimeRangeSelector time_range={time_range} setTimeRange={setTimeRange} />
       </header>
       <main className="top-artist-list-container">
-        <ol className="top-artist-list">
-          {currentArtists.map((artist, index) => (
-            <li className="top-artist-list-record" key={index}>
-              <h2 className="top-artist-list-record-text">
-                {startIndex + index + 1}. {artist.name}
-              </h2>
-            </li>
-          ))}
-        </ol>
+        <TopArtists currentArtists={currentArtists} startIndex={startIndex} />
       </main>
       <footer className="top-artist-container-footer">
-        <div className="pagination">
-          <button className="pagination-button" onClick={goToPreviousPage}>
-            &lt;
-          </button>
-          <span className="pagination-page-number">{currentPage}</span>
-          <button className="pagination-button" onClick={goToNextPage}>
-            &gt;
-          </button>
-        </div>
+        <ArtistPagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          goToPreviousPage={goToPreviousPage}
+          goToNextPage={goToNextPage}
+        />
       </footer>
     </div>
   );
 }
 
 // export function MyTopArtistCard() {
-//   let time_range = "short_term";
+//   const time_ranges = ["short_term", "medium_term", "long_term"];
+//   const [time_range, setTimeRange] = useState("medium_term");
+//   const handleTimeRangeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setTimeRange(event.target.value);
+//   };
 //   const topArtistsFullList = useGetTopArtistsFullList(time_range);
 //   const pageSize = 10;
 //   const [currentPage, setCurrentPage] = useState(1);
 //   const totalPages = Math.ceil(topArtistsFullList.length / pageSize);
 //   const startIndex = (currentPage - 1) * pageSize;
-//   const endIndex = startIndex + pageSize;
-//   const currentArtists = topArtistsFullList.slice(startIndex, endIndex);
 
-//   const goToPreviousPage = () => {
+//   const currentArtists = useMemo(() => {
+//     const startIndex = (currentPage - 1) * pageSize;
+//     const endIndex = startIndex + pageSize;
+//     return topArtistsFullList.slice(startIndex, endIndex);
+//   }, [topArtistsFullList, currentPage, pageSize]);
+
+//   const goToPreviousPage = useCallback(() => {
 //     if (currentPage > 1) {
 //       setCurrentPage(currentPage - 1);
 //     }
-//   };
+//   }, [currentPage]);
 
-//   const goToNextPage = () => {
+//   const goToNextPage = useCallback(() => {
 //     if (currentPage < totalPages) {
 //       setCurrentPage(currentPage + 1);
 //     }
-//   };
+//   }, [currentPage, totalPages]);
+
+//   useEffect(() => {
+//     setCurrentPage(1);
+//   }, [topArtistsFullList]);
 
 //   return (
 //     <div className="top-artist-container">
-//       <header className="top-artist-container-header">Top Artists</header>
+//       <header className="top-artist-container-header">
+//         Top Artists
+//         <TimeRangeRadioButtons
+//           time_ranges={time_ranges}
+//           selectedTimeRange={time_range}
+//           onTimeRangeChange={handleTimeRangeChange}
+//         />
+//       </header>
 //       <main className="top-artist-list-container">
-//         <ol className="top-artist-list">
-//           {currentArtists.map((artist, index) => (
-//             <li className="top-artist-list-record" key={index}>
-//               <h2 className="top-artist-list-record-text">
-//                 {startIndex + index + 1}. {artist.name}
-//               </h2>
-//             </li>
-//           ))}
-//         </ol>
+//         <TopArtistList currentArtists={currentArtists} startIndex={startIndex} />
 //       </main>
 //       <footer className="top-artist-container-footer">
-//         <div className="pagination">
-//           <button className="pagination-button" onClick={goToPreviousPage}>
-//             &lt;
-//           </button>
-//           <span className="pagination-page-number">{currentPage}</span>
-//           <button className="pagination-button" onClick={goToNextPage}>
-//             &gt;
-//           </button>
-//         </div>
+//         <Pagination // prettier-ignore
+//           currentPage={currentPage}
+//           goToPreviousPage={goToPreviousPage}
+//           goToNextPage={goToNextPage}
+//         />
 //       </footer>
 //     </div>
 //   );
 // }
 
 // export function MyTopArtistCard() {
-//   const [topArtists, setTopArtists] = useState<
-//     UserTopArtistsAndTracks.SpotifyItem[]
-//   >([]);
-
-//   const spotifyApi = useMemo(() => new SpotifyAPI_CS(), []); //    const spotifyApi = new SpotifyAPI_CS();
-
-//   useEffect(() => {
-//     const fetchTopArtists = async () => {
-//       const time_range = "short_term";
-//       const limit = 10;
-//       try {
-//         const response = await spotifyApi.getUserTopArtists(time_range, limit);
-//         setTopArtists(response.items);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-
-//     fetchTopArtists();
-//   }, [spotifyApi]);
-
 //   return (
 //     <div className="top-artist-container">
-//       <header className="top-artist-container-header">Top Artists</header>
+//       <header className="top-artist-container-header">
+//         Top Artists
+//         <TimeRangeSelector />
+//       </header>
 //       <main className="top-artist-list-container">
-//         <ol className="top-artist-list">
-//           {topArtists.map((artist, index) => (
-//             <li className="top-artist-list-record" key={index}>
-//               <h2 className="top-artist-list-record-text">
-//                 {index + 1}. {artist.name}
-//               </h2>
-//             </li>
-//           ))}
-//         </ol>
+//         <TopArtists />
 //       </main>
 //       <footer className="top-artist-container-footer">
-//         <div className="pagination">
-//           <button className="pagination-button">&lt;</button>
-//           <span className="pagination-page-number">1</span>
-//           <button className="pagination-button">&gt;</button>
-//         </div>
+//         <ArtistPagination />
 //       </footer>
 //     </div>
 //   );
