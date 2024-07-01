c02b04343a60d911249e2854007b2bd58ebdc14e
Chris3918 <155273681+Chris3918@users.noreply.github.com>
2024-06-30 20:36:59 -0400

    2.8 userPublicProfile

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   images: {
-    domains: ['scontent-iad3-1.xx.fbcdn.net'],
+    domains: [
+      "scontent-iad3-1.xx.fbcdn.net",
+      "scontent-atl3-1.xx.fbcdn.net",
+      "scontent-ord5-2.xx.fbcdn.net",
+      "scontent-ord5-2.xx.fbcdn.net",
+      "scontent-dfw5-2.xx.fbcdn.net",
+      "scontent-cdg4-1.xx.fbcdn.net",
+      "scontent-ord5-1.xx.fbcdn.net",
+      "scontent-iad3-2.xx.fbcdn.net",
+    ],
   },
-}
+};
--- src/app/spotify/components/userPublicProfile/userPublicProfile.tsx
+++ src/app/spotify/components/userPublicProfile/userPublicProfile.tsx
@@ -6,6 +6,9 @@ import { useUserPublicProfile } from "@/app/spotify/hooks/spotifyHooks";
 export const UserProfileComponent = ({ userId }: { userId: number }): JSX.Element => {
   const { userInfo, loading, error } = useUserPublicProfile(userId);
 
+  // Ensure that you have a fallback for when userInfo.images[1] is not available
+  const imageUrl = userInfo?.images?.[1]?.url || "/default-profile.png"; // Fallback to a default image if necessary
+
   return (
     // prettier-ignore
     loading ? <div>Loading...</div> :  
@@ -13,43 +16,26 @@ export const UserProfileComponent = ({ userId }: { userId: number }): JSX.Elemen
     !userInfo ? <div>No user information available.</div> :  
     <div className="userPublicData-container">
       <div className="userPublicData-container-userPictureRow">
-        <Image
-          src={userInfo.images[1].url}
-          width={userInfo.images[0].width}
-          height={userInfo.images[0].height}
-          alt={userInfo.display_name}
-        />{" "}
+        <div className="userPictureRow-imageWrapper">
+          <Image
+            src={imageUrl}
+            layout="fill"//"responsive"
+            objectFit="cover" // This will maintain the aspect ratio and cover the area  
+            alt={userInfo.display_name}
+            // className="userPictureRow-imageWrapper-profileImage"
+          />
+        </div>
       </div>
       {/* Image is a component from Next.js...had to create next.config.js file and add image domain */}
       <h1 className="userPublicData-container-userNameRow">{userInfo.display_name}</h1>
-      <h2>{userInfo.followers.total} followers |</h2>
-      <a href={userInfo.external_urls.spotify} target="_blank" rel="noopener noreferrer">
-        Open in Spotify
-      </a>
+      <div className="userPublicData-container-userDetailsRow">
+        <h2>{userInfo.followers.total} followers</h2>
+        <span> | </span>
+        <a href={userInfo.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+          spotify
+        </a>
+      </div>
     </div>
   );
 };
 
-// return loading ? (
-//   <div>Loading...</div>
-// ) : error ? (
-//   <div>Error: {error}</div>
-// ) : !userInfo ? (
-//   <div>No user information available.</div>
-// ) : (
-//   <div className="userPublicData-container">
-//     <Image
-//       src={userInfo.images[1].url}
-//       width={userInfo.images[0].width}
-//       height={userInfo.images[0].height}
-//       alt={userInfo.display_name}
-//     />{" "}
-//     {/* Image is a component from Next.js...had to create next.config.js file and add image domain */}
-//     <h1>{userInfo.display_name}</h1>
-//     <h2>{userInfo.followers.total} followers</h2>
-//     <a href={userInfo.external_urls.spotify} target="_blank" rel="noopener noreferrer">
-//       Open in Spotify
-//     </a>
-//   </div>
-// );
-// };
